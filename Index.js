8ff5d93cf94f73a17d9d8beb3379a076091b5e61
vincerant619 <93523700+vincerant619@users.noreply.github.com>
2022-09-19 19:19:13 +0800

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require("discord.js")
+const client = new Client({
+  intents : [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+})
+
+/* I don't recommend using and creating this constant
+If your using replit and not visual studio or any other
+IDE that's installed in your device */
+const { token } = require("./config.json")
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.login(token)
